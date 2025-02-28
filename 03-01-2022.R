6a4bbed7b32385a66d6ff33d141e4fbdbdff36f5
TzurielMurray <78871159+TzurielMurray@users.noreply.github.com>
2025-02-28 08:49:31 -0500

    Add files via upload

---
+++ 03-01-2022.R
@@ -0,0 +1,11 @@
+# Author: Tzuriel Murray, Date: 2/10/25, Purpose: Apply linear regression
+
+setwd("/Users/tzuri/Downloads")
+
+data <- read.csv(file = '03-01-2022.csv', header = TRUE)
+
+head(data)
+
+data.lm <- lm(formula = Confirmed ~ Deaths, data = data)
+
+summary(data.lm)
